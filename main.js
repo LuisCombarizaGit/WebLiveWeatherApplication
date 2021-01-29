bff49dfefced5ccb6066255aab65ee55c0ffb43a
Luis Combariza <luis_combariza@outlook.com>
2021-01-29 16:33:03 -0700

    Got api call to work and return a JSON file needed to update UI

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const api = {
-  key: "afaf9f8d48cff6cafd32e23220bcfdbf",
-  baseurl: "https//api.openweathermap.org/data/2.5",
+  key: "9241a84639ec50932c0a1c8d3b3e9434",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
-const searchbox = document.querySelector(".seach-box");
+const searchbox = document.querySelector(".search-box");
 searchbox.addEventListener("keypress", setQuery);
 
 function setQuery(evt) {
-  if (evt.KeyCode == 13) {
-    getResults(serachbox.value);
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+    console.log(searchbox.value);
   }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
 }
